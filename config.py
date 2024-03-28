0f51596bb561b468e787146161a746ccde875173
Chlain <benjaminchladni123@gmail.com>
2024-03-28 17:22:53 +0100

    replaced old with new template

--- config.py
+++
@@ -1,8 +0,0 @@
-token = 'MTIyMjY0NTk5NzE1MTMyNjI1OQ.GdLJbg.gPR20GdP95T9k-6YvLnAPtQ6G1QizbHPNjAolY' #Put Your bots token here
-
-prefix = '!' #put prefix here
-
-link = '' #put bot invite link here
-
-ownerid = '314791870838603777' #put your id here
-
