f6c815f5042254a0fc6687f7a0109943aaa0eb6a
Benjamin Chladni <benjamin.chladni@uni-ulm.de>
2024-03-29 01:16:04 +0100

    added todos for future me

--- cogs/klopfen.py
+++ cogs/klopfen.py
@@ -8,31 +8,48 @@ Version: 6.1.0
 
 from discord.ext import commands
 from discord.ext.commands import Context
+import time
+from datetime import datetime
 
 
 # Here we name the cog and create a new class for the cog.
-class Template(commands.Cog, name="template"):
+class Klopf(commands.Cog, name="klopf"):
     def __init__(self, bot) -> None:
         self.bot = bot
 
     # Here you can just add your own commands, you'll always need to provide "self" as first parameter.
 
     @commands.hybrid_command(
-        name="testcommand",
-        description="This is a testing command that does nothing.",
+        name="klopf",
+        description="This is the klopf",
     )
-    async def testcommand(self, context: Context) -> None:
+    async def klopf(self, ctx: Context) -> None:
         """
         This is a testing command that does nothing.
 
         :param context: The application command context.
         """
-        # Do your stuff here
 
-        # Don't forget to remove "pass", I added this just because there's no content in the method.
-        pass
+        '''a person is doing the klopf'''
+
+        user_roles = [r.name.lower() for r in ctx.message.author.roles]
+
+        if "Klopfer-Teilnehmer" not in user_roles:
+            pass
+            #TODO print "Du nimmst noch nicht teil, nimm teil mit kjoin um Klopfen zu können."
+        else:
+            currentTime = datetime.now().strftime(r"%I:%M %p")
+            legalTime = ["12:12 AM", "01:01 AM", "02:02 AM", "03:03 AM", "04:04 AM", "05:05 AM", "06:06 AM", "07:07 AM",
+                         "08:08 AM", "09:09 AM", "10:10 AM", "11:11 AM", "12:12 PM", "01:01 PM", "02:02 PM", "03:03 PM",
+                         "04:04 PM", "05:05 PM", "06:06 PM", "07:07 PM", "08:08 PM", "09:09 PM", "10:10 PM", "11:11 PM"]
+            if currentTime in legalTime:
+                pass
+                #TODO print "Gut gemacht! Du hast richtig geklopft"
+            else:
+                pass
+                #TODO "Duu H*** hast falsch geklopft"
 
 
 # And then we finally add the cog to the bot so that it can load, unload, reload and use it's content.
 async def setup(bot) -> None:
-    await bot.add_cog(Template(bot))
+    await bot.add_cog(Klopf(bot))
--- cogs/klopferJoin.py
+++ cogs/klopferJoin.py
@@ -11,28 +11,33 @@ from discord.ext.commands import Context
 
 
 # Here we name the cog and create a new class for the cog.
-class Template(commands.Cog, name="template"):
+class klopferJoin(commands.Cog, name="template"):
     def __init__(self, bot) -> None:
         self.bot = bot
 
     # Here you can just add your own commands, you'll always need to provide "self" as first parameter.
 
     @commands.hybrid_command(
-        name="testcommand",
-        description="This is a testing command that does nothing.",
+        name="kjoin",
+        description="With this command you join the klopf",
     )
-    async def testcommand(self, context: Context) -> None:
+    async def kjoin(self, ctx: Context) -> None:
         """
-        This is a testing command that does nothing.
+        This is a command with which the users can join the klopf
 
         :param context: The application command context.
         """
-        # Do your stuff here
 
-        # Don't forget to remove "pass", I added this just because there's no content in the method.
-        pass
+        user_roles = [r.name.lower() for r in ctx.message.author.roles]
+
+        if "Klopfer-Teilnehmer" not in user_roles:
+            member = ctx.message.author
+            # TODO: get role and give it to member
+        else:
+            pass
+            # TODO: tell the user they already participate
 
 
 # And then we finally add the cog to the bot so that it can load, unload, reload and use it's content.
 async def setup(bot) -> None:
-    await bot.add_cog(Template(bot))
+    await bot.add_cog(klopferJoin(bot))
