8ba6e781e61ae7560d88492bfb9fe85fa240aac4
Benjamin Chladni <benjamin@chladni.eu>
2026-04-19 16:30:23 +0200

    fix: dont know when the reference to the template was removed from files

--- cogs/owner.py
+++ cogs/owner.py
@@ -1,3 +1,11 @@
+"""
+Copyright © Krypton 2019-Present - https://github.com/kkrypt0nn (https://krypton.ninja)
+Description:
+🐍 A simple template to start to code your own and personalized Discord bot in Python
+
+Version: 6.5.0
+"""
+
 import discord
 from discord import app_commands
 from discord.ext import commands
